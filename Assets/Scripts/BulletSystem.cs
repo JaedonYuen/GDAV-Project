0a19de65f89e051e56245f8de3f7e9a506ce4e69
Jaedon Yuen Kit Yan <2402335J@student.tp.edu.sg>
2025-07-14 15:56:46 +0800

    Fixed more problems (Bullet system would not spawn the effect prefab) caused by improper commits

--- Assets/Scripts/BulletSystem.cs
+++ Assets/Scripts/BulletSystem.cs
@@ -4,6 +4,7 @@ public class BulletSystem : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float damage = 10f; // Damage dealt by the bullet
+    public GameObject hitEffectPrefab; // Prefab for hit effect
     void Start()
     {
 
@@ -24,5 +25,7 @@ public class BulletSystem : MonoBehaviour
             collision.gameObject.GetComponent<EnemyHealthSystem>().TakeDamage(damage);
         }
         Destroy(gameObject); // Destroy the bullet on collision
+        GameObject hitEffect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(hitEffect, 2f); // Destroy the hit effect after 2 seconds
     }
 }
