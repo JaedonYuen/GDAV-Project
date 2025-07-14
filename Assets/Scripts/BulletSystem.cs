5738de955bca7c715c2e312537938bd0053dc9de
Jaedon Yuen Kit Yan <2402335J@student.tp.edu.sg>
2025-07-14 15:51:35 +0800

    Fixed an error where the bullet system that was caused by me committng wrongly

--- Assets/Scripts/BulletSystem.cs
+++ Assets/Scripts/BulletSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BulletSystem : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float damage = 10f; // Damage dealt by the bullet
     void Start()
     {
 
@@ -16,7 +17,12 @@ public class BulletSystem : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        //should check if the bullet hit the player's gun
+        //should check if the bullet hits an enemy
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Apply damage to the enemy
+            collision.gameObject.GetComponent<EnemyHealthSystem>().TakeDamage(damage);
+        }
         Destroy(gameObject); // Destroy the bullet on collision
     }
 }
